25ff18774c9d53422484b8bd60701a566130bcf0
Sean Fisher <srtfisher@gmail.com>
2013-06-04 18:19:16 -0400

    PHP 5.3 style arrays

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ $allocator = new Allocator();
 // Adding the roles
 //$allocator->create_role('problem creator');
 $allocator->create_role('problem solver');
-$allocator->create_role('evaluator 1', ['problem creator', 'problem solver']);
-$allocator->create_role('evaluator 2', ['problem creator', 'problem solver']);
+$allocator->create_role('evaluator 1', array('problem creator', 'problem solver'));
+$allocator->create_role('evaluator 2', array('problem creator', 'problem solver'));
 
 // Assign
 try {
